18fd1d085454b9afad89be192e417453d6306caa
beyChill <74477178+beyChill@users.noreply.github.com>
2024-10-17 04:42:16 -0400

    formatting

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-from threading import Thread
 from app.database.dbactions import db_init
 from app.jsonchat import run_query_json
 from app.log.logger import setup_logging
 from app.ui.commandline import Cli
 
 
-
 if __name__ == "__main__":
-    # setup_logging()
+    setup_logging()
     db_init()
     run_query_json()
 
