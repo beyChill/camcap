ea90b914b9d299d8191ff549e199049019a81e25
beyChill <74477178+beyChill@users.noreply.github.com>
2024-10-17 04:43:46 -0400

    added subfunctions for new streamer class

--- app/ui/commandline.py
+++ app/ui/commandline.py
@@ -4,10 +4,11 @@ import sys
 from termcolor import colored
 
 from app.config.settings import get_settings
-# from app.database.dbactions import add_model
+
+
+from app.database.dbactions import add_streamer
 from app.errors.uivalidations import CliError, CliValidations
 from app.sites.chaturbate_streamer import CreateStreamer
-# from app.sites.chaturbate_streamer import aa
 
 
 config = get_settings()
@@ -22,13 +23,9 @@ class Cli(Cmd):
     def do_get(self, line) -> None:
         if (data := CliValidations().check_input(line, self.user_prompt)) is None:
             return None
-        
-        n=CreateStreamer(data.name_,data.site_slug, data.site).return_data
-        print(n.name_)
-        print(n.url)
-        print(n.file)
-        print(n.path_)
-        print(n.site)
+
+        streamer_data = CreateStreamer(data).return_data
+        add_streamer(streamer_data.name_)
 
     def do_prompt(self, new_prompt) -> None:
         try:
