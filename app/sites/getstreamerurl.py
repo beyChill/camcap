5d5476321ea3f81900adbc8ae385e0cb33baa1fc
beyChill <74477178+beyChill@users.noreply.github.com>
2024-10-25 23:01:56 -0400

    header and status_code enhancements

--- app/sites/getstreamerurl.py
+++ app/sites/getstreamerurl.py
@@ -1,29 +1,36 @@
 from logging import getLogger
 import httpx
+from httpx import AsyncClient
 from app.utils.constants import HEADERS_STREAM_URL, GetStreamerUrl
 
 log = getLogger(__name__)
 
 
 async def get_streamer_url(name_: str):
+    base_url = "https://chaturbate.com/get_edge_hls_url_ajax/"
     params = {"room_slug": name_, "bandwidth": "high"}
-    url = "https://chaturbate.com/get_edge_hls_url_ajax/"
-    async with httpx.AsyncClient(
+
+    async with AsyncClient(
         headers=HEADERS_STREAM_URL, params=params, http2=True
     ) as client:
         headers = {"Referer": f"https://chaturbate.com/{name_}/"}
 
         response = await client.post(
-            url,
+            base_url,
             headers=headers,
             data=params,
             timeout=15,
         )
+
+        if response.status_code == 429:
+            log.error(
+                f"code: {response.status_code} - too many request, try streamer capture at a later time",
+            )
+            return GetStreamerUrl(None, None, None, response.status_code)
+
         if response.status_code != 200:
             log.error(
-                "code:",
-                response.status_code,
-                "- too many request, try streamer capture at a later time",
+                f"code: {response.status_code} - failed to obtain streaming url",
             )
             return GetStreamerUrl(None, None, None, response.status_code)
 
